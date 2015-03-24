cd380fff39f9b197e586d2c6c3a5b5e0bb88c3d2
wdmg <wdmg.com.ua@gmail.com>
2015-03-24 20:54:27 +0200

    EN: Adding language files (Russian, Ukrainian) for i18n-translate interface. RU: Добавление языковых файлов (русский язык, украинский язык) для i18n-перевода интерфейса.

---
+++ backend/messages/ua/app.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Message translations.
+ * Each array element represents the translation (value) of a message (key).
+ * If the value is empty, the message is considered as not translated.
+ * Messages that no longer need translation will have their translations
+ * enclosed between a pair of '@@' marks.
+ *
+ * NOTE, this file must be saved in UTF-8 encoding.
+ *
+ * @version $Id: $
+ */
+return array (
+    'Authorization'=>'Авторизація',
+    'User authorization'=>'Авторизація користувача',
+    'Login'=>'Логін',
+);
+
+?>
