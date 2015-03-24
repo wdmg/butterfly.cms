b8deb7b4a3c65105cf3f3800b787e8c3d4a553ee
wdmg <wdmg.com.ua@gmail.com>
2015-03-24 20:40:06 +0200

    EN: Adding language settings, time zone, encoding and version of the application in the global configuration file. RU: Добавление параметров языка, временной зоны, кодировки и версии приложения в глобальный файл конфигурации.

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,23 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'i18n' => [
+            'translations' => [
+                'app*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    //'basePath' => '@app/messages',
+                    //'sourceLanguage' => 'en-US',
+                    'fileMap' => [
+                        'app' => 'app.php',
+                        'app/error' => 'error.php',
+                    ],
+                ],
+            ],
+        ],
     ],
+    'sourceLanguage'=>'en-US',
+    'language'=>'ru',
+    'charset'=>'utf-8',
+    'timeZone' => 'Europe/Kiev',
+    'version' => '1.0',
 ];
