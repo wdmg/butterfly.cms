a2735473f4b49cba62fa649fb6c92d691274ada6
wdmg <wdmg.com.ua@gmail.com>
2015-03-24 23:37:30 +0200

    Revert "EN: Set custom layout for login page (views/layouts/login.php)"
    This reverts commit ba9f0d7db90b8e0c0940525e728cb5570e1e4d26.

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -15,7 +15,6 @@ class SiteController extends Controller
     /**
      * @inheritdoc
      */
-    public $layout = 'main';
     public function behaviors()
     {
         return [
@@ -69,7 +68,6 @@ class SiteController extends Controller
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
             return $this->goBack();
         } else {
-            $this->layout = 'login'; // Set custom layout for login page (views/layouts/login.php)
             return $this->render('login', [
                 'model' => $model,
             ]);
